b1acf037d4ea075d48afb0cd3aa365338f177abd
samkitano <sam.kitano@gmail.com>
2022-12-25 20:49:14 +0000

    thinned setup bars

--- main.py
+++ main.py
@@ -141,13 +141,13 @@ def clear_yellow():
 
 def set_minutes():
     clear_yellow()
-    ssd.fill_rect(14, set_y, 48, 8, 1)
+    ssd.fill_rect(14, set_y, 48, 4, 1)
     ssd.show()
 
 
 def set_seconds():
     clear_yellow()
-    ssd.fill_rect(74, set_y, 48, 8, 1)
+    ssd.fill_rect(74, set_y, 48, 4, 1)
     ssd.show()
 
 
